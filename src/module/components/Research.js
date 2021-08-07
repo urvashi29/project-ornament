2c958cd181adda52e9bb62a74e58529fc4f221ae
urvashi29 <urvashigolu29@gmail.com>
2021-08-07 21:24:33 +0530

    final width

--- src/module/components/Research.js
+++ src/module/components/Research.js
@@ -7,12 +7,38 @@ class Research extends Component {
 			details: details,
     }
 }
-    state = {}
-    render() {
-        return(
-            <div>
 
+    render() {
+        const detailsList = details.map((info) => {
+			return (
+				<div className="card" key={info.id} id="cards">
+					<img src={info.img} className="card-img-top" alt="profile" />
+					<div className="card-body" id="card-text">
+						<div className="card-title">{info.name}</div>
+						Email: {info.mail}
+						<br />
+						Department: {info.dept}
+						<button
+							type="button"
+							id="button"
+							See Profile>
+						</button>
+					</div>
+				</div>
+			);
+		});
+        return(
+            <div className="container-fluid">
+            <div id="searchBar">
+                <input
+                    className="inputName"
+                    type="text"
+                    // onChange={this.searchHandler}
+                    placeholder="  Search"
+                />
             </div>
+            {detailsList}
+        </div>
         )
     }
 }
