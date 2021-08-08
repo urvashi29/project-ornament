b08a2622919e84b58be678e519d0707161241640
urvashi29 <urvashigolu29@gmail.com>
2021-08-08 13:29:38 +0530

    research page

--- src/module/components/Research.js
+++ src/module/components/Research.js
@@ -1,44 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import details from '../../data'
+import './Research.css';
 
 class Research extends Component {
     constructor() {
-    super(props);
-		this.state = {
-			details: details,
+        super();
+        this.state = {
+            details: details,
+        }
     }
-}
 
     render() {
+        console.log(this.state.deatils);
         const detailsList = details.map((info) => {
-			return (
-				<div className="card" key={info.id} id="cards">
-					<img src={info.img} className="card-img-top" alt="profile" />
-					<div className="card-body" id="card-text">
-						<div className="card-title">{info.name}</div>
-						Email: {info.mail}
-						<br />
-						Department: {info.dept}
-						<button
-							type="button"
-							id="button"
-							See Profile>
-						</button>
-					</div>
-				</div>
-			);
-		});
-        return(
+            return (
+                <div className="card" key={info.id} id="cards">
+                    <img src={info.img} className="card-img-top" alt="profile" />
+                    <div className="card-body" id="card-text">
+                        <h5 className="card-title">{info.name}</h5>
+                        Shape: {info.shape}
+                        <br />
+                        By: {info.genre}
+                        <br></br>
+                        <button
+                            type="button"
+                            id="button"
+                            class="btn btn-warning">
+                            View Details
+                        </button>
+                    </div>
+                </div>
+            );
+        });
+        return (
             <div className="container-fluid">
-            <div id="searchBar">
-                <input
-                    className="inputName"
-                    type="text"
-                    // onChange={this.searchHandler}
-                    placeholder="  Search"
-                />
+            <h2>Seach Database</h2>
+                <div id="searchBar">
+                    <input
+                        className="inputName"
+                        type="text"
+                        placeholder="Search for Shape"
+                    />
+                    <input
+                        className="inputName"
+                        type="text"
+                        placeholder="Search for Location"
+                    />
+                    <input
+                        className="inputName"
+                        type="text"
+                        placeholder="Search for Painter"
+                    />
+                </div>
+                <div>
+                    {detailsList}
+                </div>
+
             </div>
-            {detailsList}
-        </div>
         )
     }
 }
