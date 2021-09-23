b0aae267e7a342b2e75e387f1a08f49895be6a00
urvashi29 <urvashigolu29@gmail.com>
2021-09-23 16:59:09 +0530

    merged footer

--- src/module/container/Footer.js
+++ src/module/container/Footer.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.css";
+import apiKey from "../../emailkey";
+import{ init} from 'emailjs-com';
+import emailjs from "emailjs-com"
+init("user_xhQE8RcVRRuqKv43qLfqB");
 
 const Footer = () => {
+  const [input,setInput]=useState('')
+  const [name,setName]=useState('')
+  const handleClick=(e)=>{
+    e.preventDefault(); // Prevents default refresh by the browser
+emailjs.send(apiKey.SERVICE_ID, apiKey.TEMPLATE_ID,{name: name,input: input}, apiKey.USER_ID)
+.then((result) => {
+alert("Message Sent, We will get back to you shortly", result.text);
+},
+(error) => {
+alert("An error occurred, Please try again", error.text);
+});
+
+  }
   return (
     <div className="footer">
       <div className="contact">
@@ -19,6 +36,9 @@ const Footer = () => {
         </ul>
       </div>
       <ul className="term-policy">
+        <input type='text' onChange={e=>setName(e.target.value)} placeholder="Your Name or Email"/>
+        <input type="text" onChange={e=>setInput(e.target.value)} placeholder="Your Feedback or Query"/>
+        <button className="button-submit" onClick={(e)=>handleClick(e)}>Send</button>
         <li>
           <a href="https://latrobe.edu.au" target="_blank">
             Link to Latrobe
