6f41d16fd1c1cf8d68e4151269d2f0decb97a441
urvashi29 <urvashigolu29@gmail.com>
2021-10-04 13:33:38 +0530

    footer fixed

--- src/module/container/Footer.js
+++ src/module/container/Footer.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 import "./Footer.css";
 import apiKey from "../../emailkey";
-import{ init} from 'emailjs-com';
+import { init } from 'emailjs-com';
 import emailjs from "emailjs-com"
 init("user_xhQE8RcVRRuqKv43qLfqB");
 
 const Footer = () => {
-  const [input,setInput]=useState('')
-  const [name,setName]=useState('')
-  const handleClick=(e)=>{
+  const [input, setInput] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const handleClick = (e) => {
     e.preventDefault(); // Prevents default refresh by the browser
-emailjs.send(apiKey.SERVICE_ID, apiKey.TEMPLATE_ID,{name: name,input: input}, apiKey.USER_ID)
-.then((result) => {
-alert("Message Sent, We will get back to you shortly", result.text);
-},
-(error) => {
-alert("An error occurred, Please try again", error.text);
-});
+    emailjs.send(apiKey.SERVICE_ID, apiKey.TEMPLATE_ID, { name: name, input: input }, apiKey.USER_ID)
+      .then((result) => {
+        alert("Message Sent, We will get back to you shortly", result.text);
+      },
+        (error) => {
+          alert("An error occurred, Please try again", error.text);
+        });
+    setInput('');
+    setName('');
+  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    alert('Subscribed!');
+    setEmail('');
   }
   return (
     <div className="footer">
@@ -36,9 +45,16 @@ alert("An error occurred, Please try again", error.text);
         </ul>
       </div>
       <ul className="term-policy">
-        <input type='text' onChange={e=>setName(e.target.value)} placeholder="Your Name or Email"/>
-        <input type="text" onChange={e=>setInput(e.target.value)} placeholder="Your Feedback or Query"/>
-        <button className="button-submit" onClick={(e)=>handleClick(e)}>Send</button>
+        <input type='text' onChange={e => setName(e.target.value)} value={name} placeholder="Your Name or Email" />
+        <input type="text" onChange={e => setInput(e.target.value)} value={input} placeholder="Your Feedback or Query" />
+        <button className="button-submit" onClick={(e) => handleClick(e)}>Send</button>
+        <br />
+        <p className="subscribe">Subscribe to our Newsletter</p>
+
+        <form onSubmit={handleSubmit}>
+          <input type='text' onChange={e => setEmail(e.target.value)} value={email} placeholder="Enter Your Email" />
+          <button className="button-sub" value="submit">Subscribe</button>
+        </form>
         <li>
           <a href="https://latrobe.edu.au" target="_blank">
             Link to Latrobe
@@ -51,8 +67,16 @@ alert("An error occurred, Please try again", error.text);
         </li>
         <li>Terms of use</li>
         <li>Privacy Policy</li>
-        <li className="copyright">2021 Rights Reserved</li>
+        <li className="copyright">&copy; 2021 Rights Reserved</li>
+        <div>
+          <a href="https://www.facebook.com/latrobe" class="fa fa-facebook" target="_blank"></a>
+          <a href="https://www.linkedin.com/school/la-trobe-university/" class="fa fa-linkedin" target="_blank"></a>
+          <a href="https://www.youtube.com/channel/UCiooRETYlTQuoG8iWIxWPWg" class="fa fa-youtube" target="_blank"></a>
+          <a href="https://www.instagram.com/latrobeuni/" class="fa fa-instagram" target="_blank"></a>
+          <a href="https://www.pinterest.com/pin/77757531057016278/" class="fa fa-pinterest" target="_blank"></a>
+        </div>
       </ul>
+
     </div>
   );
 };
